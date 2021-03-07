518785d6ba78755ce54670a38cbd0fc44b97eefb
root <root@localhost.localdomain>
2021-03-07 16:50:37 -0500

    My first commit for the project

---
+++ linux.sh
@@ -0,0 +1,27 @@
+My first commit for the project, EPI
+
+echo "Please enter your name:"
+read name
+echo "Reading.... please wait"
+sleep 3 
+echo "-----------------------------"
+echo "Your username is:" && whoami 
+echo "-----------------------------"
+echo "This is one of my first Linux Scripts"
+sleep 2
+echo "Please wait while we process the date......."
+sleep 3
+echo "Made on:" && date
+echo "-----------------------------"
+sleep 2
+echo "My IP address is:"
+echo "-----------------------------"
+sleep 2
+curl ifconfig.me
+
+This is Eric Ingram. I made a change to Joel McKenzies file, I think I am on the right track....
+
+
+add a new line
+add anoter line
+add a test env
